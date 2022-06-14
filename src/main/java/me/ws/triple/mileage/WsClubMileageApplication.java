5df9057b66f66cdc7697b1d408499c5af244a251
woosung-Lee <64631529+woosung-Lee@users.noreply.github.com>
2022-06-14 10:15:15 +0900

    Initial commit

---
+++ src/main/java/me/ws/triple/mileage/WsClubMileageApplication.java
@@ -0,0 +1,13 @@
+package me.ws.triple.mileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsClubMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsClubMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ws/triple/mileage/WsClubMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ws.triple.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsClubMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
